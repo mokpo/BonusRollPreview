761e67166d8da863e17b7f9889d1e64547c3833b
mokpo <spdlxm@gmail.com>
2019-02-09 01:55:27 +0900

    update koKR.lua

--- locale/koKR.lua
+++ locale/koKR.lua
@@ -1,8 +1,8 @@
 local L = select(2, ...)('koKR')
 
 -- Config
-L['Lock/unlock movement with /brp lock, and reset the position with /brp reset.'] = 'Lock/unlock movement with /brp lock, and reset the position with /brp reset.' -- needs translation
-L['Always unfold the loot list'] = 'Always unfold the loot list' -- needs translation
-L['Direction the loot list should appear'] = 'Direction the loot list should appear' -- needs translation
-L['Up'] = 'Up' -- needs translation
-L['Down'] = 'Down' -- needs translation
+L['Lock/unlock movement with /brp lock, and reset the position with /brp reset.'] = '/brp lock 잠금/잠금해제, /brp reset으로 위치를 재설정하십시오.'
+L['Always unfold the loot list'] = '전리품 목록을 항상 펼칩니다.'
+L['Direction the loot list should appear'] = "전리품 목록의 방향'
+L['Up'] = '위'
+L['Down'] = '아래'
